de98fce7e6382603ac897b224c0f8e03725e18e8
David Osorio <dravix@gmail.com>
2016-12-15 21:47:19 -0600

    Se establecio la estructura visual con bootstrap

---
+++ application/controllers/AppController.php
@@ -0,0 +1,25 @@
+<?php
+
+class AppController extends Zend_Controller_Action
+{
+
+    public function init()
+    {
+        /* Initialize action controller here */
+    }
+
+    public function indexAction()
+    {
+        // action body
+    }
+
+    public function addAction()
+    {
+        // Agrega un contacto
+    }
+
+
+}
+
+
+
