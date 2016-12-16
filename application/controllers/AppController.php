0bc00429e4f158351e93833731a59c6a0d32b51e
David Osorio <dravix@gmail.com>
2016-12-16 14:25:20 -0600

    Se agrego el formulario para dar de alta sin ajax

--- application/controllers/AppController.php
+++ application/controllers/AppController.php
@@ -18,11 +18,46 @@ class AppController extends Zend_Controller_Action
     public function addAction()
     {
         // Agrega un contacto
-        $this->_helper->layout()->disableLayout();
+        if ($this->getRequest()->isXmlHttpRequest()) {
 
+            $this->_helper->layout()->disableLayout();
+        }
+        $form = new Application_Form_Contacto;
+        $form->submit->setLabel('Guardar');
+        $this->view->form = $form;
+        if ($this->getRequest()->isPost()) {
+           $formData = $this->getRequest()->getPost();
+           if ($form->isValid($formData)) {
+               $nombre = $form->getValue('Nombre');
+               $correo = $form->getValue('Email');
+               $direccion = $form->getValue('Direccion');
+               $contacto = new Application_Model_DbTable_Contacto();
+               $contacto->agregar($nombre, $correo,$direccion);
 
+               $this->_helper->redirector('index');
+           } else {
+               $form->populate($formData);
+           }
+       }
 
     }
+
+    public function saveAction()
+    {
+        if ($this->getRequest()->isPost()) {
+           $formData = $this->getRequest()->getPost();
+           if ($form->isValid($formData)) {
+               $nombre = $form->getValue('Nombre');
+               $correo = $form->getValue('Email');
+               $direccion = $form->getValue('Direccion');
+               $contacto = new Application_Model_DbTable_Contacto();
+               $contacto->agregar($nombre, $correo,$direccion);
+
+               return json_encode(true);
+           } 
+       }
+       return json_encode(false);
+   }
 
     public function listaAction()
     {
